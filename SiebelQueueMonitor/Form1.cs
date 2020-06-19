57525e146038983691e33cce1748d77a307ec261
Greg Cunningham <greg.h.cunningham@outlook.com>
2020-06-19 01:31:43 -0400

    Got subpages working -WIP

--- SiebelQueueMonitor/Form1.cs
+++ SiebelQueueMonitor/Form1.cs
@@ -12,10 +12,69 @@ namespace SiebelQueueMonitor
 {
     public partial class Form1 : Form
     {
+        Timer t = new Timer();
+
         public Form1()
         {
             InitializeComponent();
-            selectedMenu("default");
+            selectedMenu("default");            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer interval
+            t.Interval = 1000;  //in milliseconds
+
+            t.Tick += new EventHandler(this.clock_Tick);
+
+            //start timer when form loads
+            t.Start();  //this will use t_Tick() method
+        }
+
+        private void clock_Tick(object sender, EventArgs e)
+        {            
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+            
+            string time = "";
+
+            int hh_prefernce;
+            if (hh < 12)
+                hh_prefernce = (hh - 12);
+            else
+                hh_prefernce = hh;
+
+            if (hh_prefernce < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+                            
+            time += ":";
+
+            if (mm < 10)            
+                time += "0" + mm;            
+            else
+            
+                time += mm;     
+            
+            time += ":";
+
+            if (ss < 10)            
+                time += "0" + ss;            
+            else            
+                time += ss;
+
+            if (hh < 12)
+                time += " AM";
+            else
+                time += " PM";
+
+            clockDisplay.Text = time;
         }
 
         private void selectedMenu(string nm)
@@ -44,11 +103,12 @@ namespace SiebelQueueMonitor
         private void btnDashboard_Click(object sender, EventArgs e)
         {
             selectedMenu("default");
+            openChildForm(new Dashboard());
         }
 
         private void btnReports_Click(object sender, EventArgs e)
         {
-            selectedMenu("reports");
+            selectedMenu("reports");            
         }
 
         private void btnManage_Consultants_Click(object sender, EventArgs e)
@@ -59,6 +119,20 @@ namespace SiebelQueueMonitor
         private void btnSettings_Click(object sender, EventArgs e)
         {
             selectedMenu("settings");
+        }
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null) activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
         }
     }
 }
